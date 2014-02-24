28be2b4a6fcb72bea677ca892619e51bbde55874
mprat <michele.pratusevich@gmail.com>
2014-02-24 14:27:38 +0200

    more modular

--- button.py
+++ button.py
@@ -3,31 +3,42 @@ import fruit
 import label
 
 class Button(pygame.Surface):
-    def __init__(self, x_corner, y_corner, x, y, img_filename=""):
+    def __init__(self, x_corner, y_corner, x, y, text="", img_filename=""):
         pygame.Surface.__init__(self, size=(x, y))
         self.rec = pygame.Rect(x_corner, y_corner, x, y)
         self.img = ""
+        self.text = ""
+        self.x_corner = x_corner
+        self.y_corner = y_corner
+        self.x = x
+        self.y = y
         if img_filename:
             self.img = pygame.image.load(img_filename)
+        if text: 
+            self.text = text
 
     def draw(self, screen):
         if self.img: 
             screen.blit(self.img, self.rec)
         else: 
-            screen.blit(self, self.rec)
+            # screen.blit(self, self.rec)
+
+            if self.text: 
+                l = label.Label(30, self.x_corner, self.y_corner, self.x, self.y)
+                l.draw(self.text, screen)
+            else: 
+                screen.blit(self, self.rec)
 
     def click(self):
         print "button clicked"
 
 class AppleButton(Button):
-    def click(self, screen):
-        a = fruit.Fruit("Apple", 5)
+    def click(self, screen, num):
         l = label.Label(30, 50, 50, 200, 30)
-        l.draw(str(a) + "      ", screen)
+        l.draw("Apples: " + str(num), screen)
 
 
 class BananaButton(Button):
-    def click(self, screen):
-        a = fruit.Fruit("Banana", 10)
-        l = label.Label(30, 50, 50, 200, 30)
-        l.draw(str(a), screen)
+    def click(self, screen, num):
+        l = label.Label(30, 50, 100, 200, 30)
+        l.draw("Bananas: " + str(num), screen)
--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import pygame
 import sys
 import fruit
 import button
+import label
 
 if __name__=="__main__":
     pygame.init()
     screen_size = 400
     main_screen = pygame.display.set_mode((screen_size, screen_size))
     main_screen.fill((255,255,255))
-    label_rec = pygame.Rect(50, 50, 200, 30)
-    orderlabel = pygame.font.Font(None, 30)
 
     b1 = button.BananaButton(0, 0, 40, 40, 'baby-bamba.jpg')
     b2 = button.AppleButton(100, 0, 40, 40)
     b1.draw(main_screen)
     b2.draw(main_screen)
+
+    checkoutbtn = button.Button(300, 200, 50, 50)
+    checkoutbtn.fill((255, 0, 0))
+    checkoutbtn.draw(main_screen)
+
+    donelabel = label.Label(30, 50, 300, 200, 30)
+    donelabel.draw("no checkout", main_screen)
+
+    newbtn = button.Button(200, 0, 40, 40)
+
+    checkout = []
+    numapples = 0
+    numbananas = 0
 
     while True:
         event = pygame.event.poll()
@@ -23,9 +35,15 @@ if __name__=="__main__":
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
             if b1.rec.collidepoint(x, y):
-                b1.click(main_screen)
+                numapples += 1
+                checkout.append(fruit.Fruit("Apple", 5))
+                b1.click(main_screen, numapples)
             if b2.rec.collidepoint(x, y):
-                b2.click(main_screen)
+                numbananas += 1
+                checkout.append(fruit.Fruit("Banana", 10))
+                b2.click(main_screen, numbananas)
+            if checkoutbtn.rec.collidepoint(x, y):
+                donelabel.draw(str(numapples) + " apples and " + str(numbananas) + " bananas", main_screen)
         pygame.display.flip()
  
 	# pygame.quit()
